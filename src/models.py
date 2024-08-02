0b102d29d90c0ae3b6206d4ff5a021b230f42c30
Dylan Shafe <143671996+Knightlife27@users.noreply.github.com>
2024-08-02 20:43:04 +0000

    second commit

--- src/models.py
+++ src/models.py
@@ -7,26 +7,50 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+# class Person(Base):
+#     __tablename__ = 'person'
+#     # Here we define columns for the table person
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
 
-    def to_dict(self):
-        return {}
+#     def to_dict(self):
+#         return {}
+
+class User(Base):
+    id = Column(Integer, primary_key=True)
+    userName = Column(String(32), nullable = False)
+    password = Column(String(32), nullable = False)
+
+class Character(Base):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(32), nullable = False)
+    weapon = Column(String(32), nullable = False)
+
+class Planet(Base):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(32), nullable = False)
+    gravity = Column(String(32), nullable = False)
+    climate = Column(String(32), nullable = False)
+
+class Vehicles(Base):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(32), nullable = False)
+    weapon = Column(String(32), nullable = False)
+    faction = Column(String(32), nullable = False)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
+
+
