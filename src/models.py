4cefe512e3d27f4f8ae7df9fe0653b8c06047257
Dylan Shafe <143671996+Knightlife27@users.noreply.github.com>
2024-10-07 18:40:41 +0000

    update

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
@@ -28,23 +28,51 @@ Base = declarative_base()
 #     def to_dict(self):
 #         return {}
 
+favorite_characters = Table(
+    "favorite_characters",
+    Base.metadata,
+    Column("user_id", ForeignKey("user.id")),
+    Column("character_id", ForeignKey("character.id")),
+)
+
+favorite_planets = Table(
+    "favorite_planets",
+    Base.metadata,
+    Column("user_id", ForeignKey("user.id")),
+    Column("planet_id", ForeignKey("planet.id")),
+)
+
+favorite_vehicles = Table(
+    "favorite_vehicles",
+    Base.metadata,
+    Column("user_id", ForeignKey("user.id")),
+    Column("vehicle_id", ForeignKey("vehicle.id")),
+)
+
 class User(Base):
+    __tablename__= "user"
     id = Column(Integer, primary_key=True)
     userName = Column(String(32), nullable = False)
     password = Column(String(32), nullable = False)
+    favorite_characters = relationship("Character", secondary=favorite_characters)
+    favorite_planets = relationship("Planet", secondary=favorite_planets)
+    favorite_vehicles = relationship("Vehicle", secondary=favorite_vehicles)
 
 class Character(Base):
+    __tablename__= "character"
     id = Column(Integer, primary_key=True)
     name = Column(String(32), nullable = False)
     weapon = Column(String(32), nullable = False)
 
 class Planet(Base):
+    __tablename__= "planet"
     id = Column(Integer, primary_key=True)
     name = Column(String(32), nullable = False)
     gravity = Column(String(32), nullable = False)
     climate = Column(String(32), nullable = False)
 
-class Vehicles(Base):
+class Vehicle(Base):
+    __tablename__= "vehicle"
     id = Column(Integer, primary_key=True)
     name = Column(String(32), nullable = False)
     weapon = Column(String(32), nullable = False)
